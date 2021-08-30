83c3b6632085b4e6c57d5ea7c2ca431a8a087549
codedao360 <codedao360@gmail.com>
2021-08-30 16:29:12 +0700

    Adding some card

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Header from './components/Header';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Gues a number" />
+      <StartGameScreen />
     </View>
   );
 }
